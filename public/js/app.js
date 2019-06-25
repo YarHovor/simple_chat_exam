7d10215075c2c56504daf4aac063f22965d53691
YarHovor <yaroslav.hovorukha@gmail.com>
2019-06-25 13:23:33 +0300

    simple_chat_exam v2.0 (4/5--+)

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,7 @@ $('.js-send').on('click', function (event) {
    event.preventDefault();
 
    $.get(this.href, function (data) {
-        $('.')
+        $('#js-answer-table').html(data);
    });
-});*/
+});
+*/
